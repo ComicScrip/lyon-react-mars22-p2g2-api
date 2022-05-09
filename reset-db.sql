7775a4f343a7363c042d4b8834dbdb3ef73ae1e1
QuentinJOUANNE <QUENTIN.JOUANNE94@gmail.com>
2022-05-09 15:05:44 +0200

    add reset file and change table settings

---
+++ reset-db.sql
@@ -0,0 +1,37 @@
+[
+    {
+        "id": 1,
+        "userName": "Brigitte",
+        "movieName": "taken",
+        "location": "Lyon",
+        "date": "2022-05-02T22:00:00.000Z"
+    },
+    {
+        "id": 3,
+        "userName": "Quentin",
+        "movieName": "Spartatouille",
+        "location": "Lyon 7",
+        "date": "2022-05-05T22:00:00.000Z"
+    },
+    {
+        "id": 2,
+        "userName": "Julie",
+        "movieName": "Twilight",
+        "location": "Lyon 6",
+        "date": "2022-05-21T22:00:00.000Z"
+    },
+    {
+        "id": 4,
+        "userName": "Benjamain",
+        "movieName": "Drive Indian",
+        "location": "Lyon 9",
+        "date": "2022-09-21T22:00:00.000Z"
+    },
+    {
+        "id": 5,
+        "userName": "Pierre",
+        "movieName": "Problemos",
+        "location": "Lyon 7",
+        "date": "2022-10-13T22:00:00.000Z"
+    }
+]
--- src/app.js
+++ src/app.js
@@ -1,15 +1,97 @@
+/* eslint-disable curly */
 const express = require('express');
-
+const connection = require('./db-config');
+const db = require('./db-config');
+const Joi = require('joi');
 const app = express();
 
-const items = [
-  { id: 1, name: 'item1' },
-  { id: 2, name: 'item2' },
-];
+connection.connect((err) => {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+  } else {
+    console.log('connected as id ' + connection.threadId);
+  }
+});
 
-app.get('/things', (req, res) => {
-  console.log('handling GET /things');
-  res.send(items);
+app.use(express.json());
+
+// Afficher les disponibilités
+app.get('/availabilities', async (req, res) => {
+  try {
+    const [availabilities] = await db
+      .promise()
+      .query('SELECT * FROM availabilities ORDER BY `date` ASC');
+    res.send(availabilities);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('something wrong happened');
+  }
+});
+
+app.get('/availabilities/:id', (req, res) => {
+  const availabilitiesId = req.params.id;
+  connection.query(
+    'SELECT * FROM availabilities WHERE id = ?',
+    [availabilitiesId],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error retrieving availabilities from database');
+      } else if (result.length === 0) {
+        res.status(404).send('Availabilities not found');
+      } else {
+        res.json(result[0]);
+      }
+    }
+  );
+});
+// Poster une disponibilité.
+app.post('/availabilities', async (req, res) => {
+  try {
+    const { userName, movieName, location, date } = req.body;
+    const { error: validationErrors } = Joi.object({
+      userName: Joi.string().max(50).required(),
+      movieName: Joi.string().max(50).required(),
+      location: Joi.string().max(50).required(),
+      date: Joi.string().max(50).required(),
+    }).validate({ userName, movieName, location, date }, { abortEarly: false });
+
+    if (validationErrors) {
+      return res.status(422).json({ errors: validationErrors.details });
+    }
+    const [{ insertId }] = await db
+      .promise()
+      .query(
+        'INSERT INTO availabilities (userName, movieName, location, date ) VALUES (?, ?, ?, ?)',
+        [userName, movieName, location, date]
+      );
+
+    res.status(201).send({ id: insertId, userName, movieName, location, date });
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+db.connect((err) => {
+  if (err) console.error('error connecting to db');
+});
+
+// Pour supprimer une disponibilité
+app.delete('/availabilities/:id', (req, res) => {
+  connection.query(
+    'DELETE FROM availabilities WHERE id = ?',
+    [req.params.id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error deleting an availabilities');
+      } else {
+        if (result.affectedRows)
+          res.status(200).send('🎉 availabilities deleted!');
+        else res.status(404).send('Availabilities not found.');
+      }
+    }
+  );
 });
 
 module.exports.app = app;
